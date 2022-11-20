1734963eaa499455a0167ac21e2ce1fabfebc4eb
N-Sirun <88889025+N-Sirun@users.noreply.github.com>
2022-11-20 20:24:38 +0400

    file

---
+++ 1Ex.py
@@ -0,0 +1,46 @@
+ #Ex 2
+# num1 = int(input("Enter number 1 : "))
+# num2 = int(input("Enter number 2 : "))
+#
+# print("Addition: ",(num1+num2))
+# print("Subtraction: ",(num1-num2))
+# print("Multiplication: ",(num1*num2))
+# print("Division: ",(num1/num2))
+
+ #Ex 3
+# import math
+# number = int(input("Enter any number : "))
+# print("Square root of ", number, " is ", math.sqrt(number))
+
+ # Ex 4
+# x = int(input("Enter value of x: "))
+# y = int(input("Enter value of y: "))
+# temp = x
+# x = y
+# y = temp
+# print("The value of x after swapping is {}".format(x))
+# print("The value of y after swapping is {}".format(y))
+# x, y = y, x
+# print("The value of x after swapping is ", x)
+# print("The value of y after swapping is ", y)
+
+ #Ex 5
+# for n in range(7):
+#      print("Hello")
+#print("Hello\n" * 7)
+
+
+ #Ex 6
+# import random
+# print(random.randint(1, 100))
+
+ #Ex 7
+# num = int(input("Enter any number to test whether it is odd or even: " ))
+# if (num % 2) == 0:
+#     print(num, " is even")
+# else:
+#     print(num, " is odd")
+
+ #Ex 8
+my_list = [2, 5, 6, 84, 240, 25, 152, 421, 325]
+print(max(my_list))
